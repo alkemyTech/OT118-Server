cca08bf2b836c224f684b66a666b63b8786279c8
Sebastián Arca <sebastian.arca@sitrack.com>
2021-12-17 12:50:43 -0300

    OT118-231 adding input validation middleware to create news

--- routes/news.js
+++ routes/news.js
@@ -3,10 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 const newsController = require('../controllers/news');
+const newsMiddleware = require('../middlewares/news');
 const authMiddleware = require('../middlewares/auth');
 
 
-router.post('/', newsController.create);
+router.post('/', newsMiddleware.inputValidation, newsController.create);
 router.delete('/:id', authMiddleware.isAdmin, newsController.remove);
 
 module.exports = router;
