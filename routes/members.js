8461520ebd8fc7f5717b2b48b99bc3579645a6d1
IvanStacul <stacul.ivan98@gmail.com>
2021-12-18 20:35:15 -0300

    member validations added

--- routes/members.js
+++ routes/members.js
@@ -3,9 +3,10 @@ const express = require("express");
 const router = express.Router();
 const membersController = require("../controllers/members");
 const authMiddleware = require("../middlewares/auth");
+const memberMiddleware = require("../middlewares/memberValidator");
 
 router.get("/", authMiddleware.isAdmin, membersController.getAll);
-router.post("/",  authMiddleware.isAuth, membersController.create);
+router.post( "/",  [authMiddleware.isAuth, memberMiddleware], membersController.create);
 router.delete("/:id", authMiddleware.isAdmin, membersController.remove);
 
 module.exports = router;
