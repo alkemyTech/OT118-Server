968a6457f2384de9d592e6a4dcb8f74cb563aa94
Robertlean <robert_lean@live.com.ar>
2021-12-19 20:41:26 -0300

    middleware users 213

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,7 +6,30 @@ const isAuth = async (req, res, next) => {
   throw new Error('Not implemented');
 };
 
+const isUserLogged = (req, res, next) => {
+  try {
+    const token = req.headers['authorization'];
+    const decodedToken = jwtService.verify(token);
+    if (!decodedToken) {
+      const error = {
+        status: 403,
+        msg: "Invalid Token"
+      }
+      throw error
+    }
+    next()
+  } catch (error) {
+    const error = {
+      status: 403,
+      msg: "Invalud or expire Token"
+    }
+    next(error)
+  }
+}
+
+
 module.exports = {
   isAdmin,
   isAuth,
+  isUserLogged
 };
