e23b945978b91fa0c461a32f3d064aff435639f5
Carlos Adrián Sosa <adriansosa105@gmail.com>
2021-12-20 10:48:22 -0300

    Route and controller added.

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,5 +1,15 @@
 const categoriesService = require('../services/categories');
 
+// Get all categoies
+const getAll = async (req, res, next) => {
+  try {
+    const respones = await categoriesService.getAll();
+    res.status(200).json({ msg: `Categories succsesfully listed ${categoriesService.getAll.id}`, response});
+  } catch (error) {
+    next(error);
+  }
+};
+
 const remove = async (req, res, next) => {
   try {
     await categoriesService.remove(req.params.id);
--- routes/categories.js
+++ routes/categories.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const categoriesController = require('../controllers/categories');
 const authMiddleware = require('../middlewares/auth');
 
+router.get('/', categoriesController.getAll);
 router.delete('/:id', authMiddleware.isAdmin, categoriesController.remove);
 
 module.exports = router;
