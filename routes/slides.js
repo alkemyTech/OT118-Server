9d062b89f5f6f6d4e68d7eed5afa18951d6f842b
Tabitoo <lautarovaldez4@gmail.com>
2021-12-21 17:26:24 -0300

    quick fixes

--- routes/slides.js
+++ routes/slides.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const slidesController = require('../controllers/slides');
 const authMiddleware = require('../middlewares/auth');
-const slideValidator = require("../middlewares/slideValidator")
+const slidesValidation = require('../middlewares/slideValidator')
 
 const router = express.Router();
 
-router.post('/create', slideValidator, slidesController.create)
+router.post('/create', slidesValidation.validationFields ,slidesController.create)
 router.delete('/:id', authMiddleware.isAdmin, slidesController.remove);
 
 module.exports = router;
