0e843d7827ec31309ce5a607c7f4afe8a34fa4b6
Ivan Stacul <41239723+IvanStacul@users.noreply.github.com>
2021-12-21 20:51:42 -0300

    Members endpoints (#6)
    * cookie-parser dependencie added
    
    * getAll members endpoint created
    
    * create member endpoint added (no validations)
    
    * member validations added

--- repositories/members.js
+++ repositories/members.js
@@ -1,9 +1,26 @@
-const db = require('../models');
+const db = require("../models");
+
+const getAll = async () => {
+  return await db.Members.findAll();
+};
+
+const create = async (member) => {
+  return await db.Members.create({
+    name: member.name,
+    image: member.image,
+    facebookUrl: member.facebookUrl || null,
+    instagramUrl: member.instagramUrl || null,
+    linkedinUrl: member.linkedinUrl || null,
+    description: member.description || null,
+  });
+};
 
 const remove = async (id) => {
   await db.Members.destroy({ where: { id: id } });
 };
 
 module.exports = {
-  remove
+  getAll,
+  create,
+  remove,
 };
