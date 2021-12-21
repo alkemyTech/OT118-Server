f95ae99c41641e1c0348aa7a269bce8104ea9b7f
Robertlean <32883173+Robertlean@users.noreply.github.com>
2021-12-21 20:05:11 -0300

    getAll comments 255 (#7)
    * getAll comments 255
    
    * correction of the get All comments

--- services/comments.js
+++ services/comments.js
@@ -3,7 +3,11 @@ const commentsRepository = require('../repositories/comments');
 const remove = async (id) => {
   await commentsRepository.remove(id);
 };
+const getAll = async() =>{
+  return await commentsRepository.getAll()
+}
 
 module.exports = {
-  remove
+  remove,
+  getAll
 };
