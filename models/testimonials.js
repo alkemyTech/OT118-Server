2284a5230e5018a927c6eb5fd22930de46065045
Sebastián Arca <sebasarca711@gmail.com>
2021-12-22 11:17:43 -0300

    Fixing uppercase database table names

--- models/testimonials.js
+++ models/testimonials.js
@@ -29,7 +29,6 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Testimonials',
-    tableName: 'testimonials',
     timestamps: true,
     paranoid: true
   });
