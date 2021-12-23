73b0b59b9be2c8c4f43787467550d27ecc23ec15
Emmanuel Tabares <e.tabares20@gmail.com>
2021-12-22 23:24:02 -0300

    fix Entity Activities

--- middlewares/activities.js
+++ middlewares/activities.js
@@ -18,7 +18,7 @@ const validationFields = [
     async (req, res, next) => {
         const errors = validationResult(req);
         if(!errors.isEmpty()) {
-            return res.status(422).json({ errors: errors.array() })
+            return res.status(400).json({ errors: errors.array() })
         }
         next()
     }
