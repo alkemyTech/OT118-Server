3b77fb686af1c0e7f85866d7af2f7860c2b39bd7
IvanStacul <stacul.ivan98@gmail.com>
2021-12-23 18:57:56 -0300

    change responses message

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,20 +1,20 @@
-const categoriesService = require('../services/categories');
+const categoriesService = require("../services/categories");
 
 // Create category
 const create = async (req, res, next) => {
   try {
-    const category = await categoriesService.create(req.body);
-    res.status(201).json({msg: `Category ${category.name} created succesfully`, category});
+    const data = await categoriesService.create(req.body);
+    res.status(201).json({ msg: `Category ${category.name} created succesfully`, data });
   } catch (error) {
     next(error);
   }
-}
+};
 
 // Get all categoies
 const getAll = async (req, res, next) => {
   try {
-    const response = await categoriesService.getAll();
-    res.status(200).json(response);
+    const data = await categoriesService.getAll();
+    res.status(200).json({ data });
   } catch (error) {
     next(error);
   }
@@ -23,7 +23,9 @@ const getAll = async (req, res, next) => {
 const remove = async (req, res, next) => {
   try {
     await categoriesService.remove(req.params.id);
-    res.status(200).json({ msg: `Category ${req.params.id} removed succesfully` });
+    res
+      .status(200)
+      .json({ msg: `Category ${req.params.id} removed succesfully` });
   } catch (error) {
     next(error);
   }
@@ -32,5 +34,5 @@ const remove = async (req, res, next) => {
 module.exports = {
   create,
   getAll,
-  remove
+  remove,
 };
--- middlewares/categoryValidator.js
+++ middlewares/categoryValidator.js
@@ -10,7 +10,7 @@ const nameOk = [
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ error: errors.array()});
+            return res.status(400).json({ msg: 'Body params invalid', error: errors.array()});
         }
         next();
     }
