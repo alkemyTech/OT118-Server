850896a1a3f67128e05dfa686213a2a869d2d322
Tabitoo <lautarovaldez4@gmail.com>
2021-12-28 15:00:37 -0300

    access token added in register method

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const usersService = require('../services/users');
 const register = async (req, res, next) => {
   try {
     const data = await usersService.create(req.body);
-    res.status(201).json({ msg: `User created succesfully`, data });
+    res.status(201).json({ msg: `User created succesfully`, access_token : data });
   } catch (error) {
     next(error);
   }
