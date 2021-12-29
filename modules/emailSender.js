d51e3fa20922b9b9e2eed1f81a308dceb57760d8
Emiliobau <emiliobau@hotmail.com>
2021-12-29 00:29:36 -0300

    sendgrid library integration

---
+++ modules/emailSender.js
@@ -0,0 +1,18 @@
+const sgMail = require("@sendgrid/mail");
+const { sendGrid_key, sendGrid_mail } = require('../config/config');
+
+sgMail.setApiKey( sendGrid_key);
+
+const send = async (email, template, subject) => {
+  const message = {
+    to: email,
+    from: sendGrid_mail,
+    subject: subject,
+    html: template,
+  };
+    await sgMail.send(message);
+  }
+
+module.exports = {
+  send
+};
