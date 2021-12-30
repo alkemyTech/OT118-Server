0d69df4d477768d4c5777f71d6db6e04ca9ab32e
Carlos Adrián Sosa <adriansosa105@gmail.com>
2021-12-29 21:40:54 -0300

    Changes requested by pull request.

--- controllers/categories.js
+++ controllers/categories.js
@@ -12,7 +12,7 @@ const create = async (req, res, next) => {
 const update = async (req, res, next) => {
   try {
     const data = await categoriesService.update(req.params.id, req.body);
-    res.status(200).json({ msg: `Category updated succesfully`, data: req.body });
+    res.status(200).json({ msg: `Category updated succesfully`, data: data });
   } catch (error) {
     next(error);
   }
