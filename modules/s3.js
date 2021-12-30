1cf9980cc4a32431e7547f79da8f528da299013c
Matias Sebastian Arca <sebasarca711@gmail.com>
2021-12-30 20:38:04 -0300

    Ot118-224 AWS S3 Upload File Module (#45)
    * OT118-224 AWS s3 Module to upload files
    
    * OT118-224 Changes to method signature and file object
    
    * OT118-224 changes to bucket Method
    
    * OT118-224 Fix config bucket name
    
    Co-authored-by: Ivan Stacul <41239723+IvanStacul@users.noreply.github.com>

---
+++ modules/s3.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+const AWS = require('aws-sdk');
+const { awsS3 } = require('../config/config');
+
+
+const s3 = new AWS.S3({
+    accessKeyId: awsS3.accessKey,
+    secretAccessKey: awsS3.secretKey,
+    region: awsS3.region,
+});
+
+const uploadToBucket = async (file) => {
+    let uploadedFile;
+    try {
+        const stream = fs.createReadStream(file.tempFilePath);
+        const extension = path.extname(file.name).toLowerCase();
+        uploadedFile = await s3.upload(
+            {
+                Bucket: awsS3.bucketName,
+                Key: uuidv4() + extension,
+                Body: stream,
+            }).promise();
+        return uploadedFile;
+    } catch (err) {
+        throw err;
+    }
+};
+
+module.exports = uploadToBucket
