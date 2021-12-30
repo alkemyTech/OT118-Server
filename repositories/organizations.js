fc7dfee67c051bb639e20e9fde6e184e12e03ad9
IvanStacul <stacul.ivan98@gmail.com>
2021-12-29 21:35:57 -0300

    contact mail template integrated

--- repositories/organizations.js
+++ repositories/organizations.js
@@ -1,12 +1,17 @@
 const db = require('../models');
 
+const getById = async (id) => {
+  return await db.Organization.findByPk(id);
+};
+
 const update = async (id, body) => {
   const data = await db.Organization.update(body, {
-    where: { id }
+    where: { id },
   });
   return data;
 };
 
 module.exports = {
-  update
+  getById,
+  update,
 };
--- services/organizations.js
+++ services/organizations.js
@@ -1,9 +1,20 @@
 const organizationRepository = require('../repositories/organizations');
 
+const getById = async (id) => {
+  const organization = await organizationRepository.getById(id);
+  if (!organization) {
+    const error = new Error(`No existe la organización`);
+    error.status = 404;
+    throw error;
+  }
+  return organization;
+};
+
 const update = async (id, body) => {
   return await organizationRepository.update(id, body);
 };
 
 module.exports = {
+  getById,
   update
 };
