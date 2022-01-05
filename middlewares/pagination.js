2700b016d14ba344a89f65f056f0e0a18caa1d50
IvanStacul <stacul.ivan98@gmail.com>
2022-01-05 20:03:58 -0300

    fix bug

--- middlewares/pagination.js
+++ middlewares/pagination.js
@@ -1,16 +1,5 @@
-const { check, validationResult } = require("express-validator");
-// const { checkValidationResults } = require("./validation");
-
-const checkValidationResults = async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      msg: "Body params invalid.",
-      errors: errors.array(),
-    });
-  }
-  next();
-};
+const { check } = require("express-validator");
+const { checkValidationResults } = require("./validation");
 
 const validator = [
   check("page", "Parameter 'page' must be an integer greater than zero").optional().isInt({ min: 1 }),
