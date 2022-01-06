15a24f3f9a0b26d11dd1b8a19c810675740aff35
Carlos Adrián Sosa <adriansosa105@gmail.com>
2022-01-06 19:45:18 -0300

    Pagination route added

--- routes/categories.js
+++ routes/categories.js
@@ -5,9 +5,10 @@ const router = express.Router();
 const categoriesController = require('../controllers/categories');
 const authMiddleware = require('../middlewares/auth');
 const categoryValidator = require('../middlewares/categoryValidator');
+const paginationMiddleware = require('../middlewares/pagination')
 
 router.post('/', authMiddleware.isAdmin, categoryValidator.nameOk, categoriesController.create);
-router.get('/', categoriesController.getAll);
+router.get('/', authMiddleware.isAdmin, paginationMiddleware.validator, categoriesController.getAll);
 router.put('/:id', authMiddleware.isAdmin, categoryValidator.nameOk, categoriesController.update);
 router.get('/:id', authMiddleware.isAdmin, categoriesController.getById);
 router.delete('/:id', authMiddleware.isAdmin, categoriesController.remove);
