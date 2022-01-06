53c04d95b94a96b7eddd1253e9608c99533326a2
Sebastián Arca <sebasarca711@gmail.com>
2022-01-06 20:36:58 -0300

    Comments Fixes

--- middlewares/comments.js
+++ middlewares/comments.js
@@ -1,4 +1,5 @@
-const {check, validationResult} = require('express-validator');
+const { check } = require('express-validator');
+const { checkValidationResults } = require("./validation");
 
 const validationNewUserId = check('novelty_id', 'News must be not empty')
     .notEmpty()
@@ -18,15 +19,7 @@ const validationFields = [
     validationNewUserId,
     validationUser,
     validationBody,
-    async (req, res, next) =>{
-        const errors = validationResult(req);
-        if(!errors.isEmpty()){
-            return res.status(422).json({
-                errors: errors.array()
-            })
-        }
-        next()
-    }
+    checkValidationResults,
 ]
 
 module.exports = {
