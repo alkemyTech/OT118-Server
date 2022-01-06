1013df3e5526b8d72cc01d18e94e34010ccc6615
Tabitoo <lautarovaldez4@gmail.com>
2022-01-06 03:44:41 -0300

    createError module added

--- services/slides.js
+++ services/slides.js
@@ -1,6 +1,14 @@
 const slidesRepository = require('../repositories/slides');
+const organizationRepository = require('../repositories/organizations');
+const createError = require('http-errors');
 
 const create = async(body) => {
+
+  const organization = await organizationRepository.getById(body.organizationId);
+
+  if(!organization){
+    throw createError(404, 'OrganizationId not found')
+  }
 
   if(body.order == undefined){
     let maxOrder = await slidesRepository.getMaxOrder()
@@ -17,11 +25,17 @@ const update = async (id, body) => {
   const slide = await slidesRepository.getById(id);
 
   if(!slide) {
-    throw new Error('slide not fund')
+    throw createError(404, 'Slide not found')
     
   }
 
-  return await slidesRepository.update(body, id)
+  const updatedSlide = await slidesRepository.update(body, id)
+
+  if(updatedSlide[0] !== 1) {
+    throw createError(400, 'Slide couldnt be updated')
+  }
+
+  return await slidesRepository.getById(id);
   
 }
 
@@ -36,14 +50,21 @@ const getById = async (id) => {
   const data = await slidesRepository.getById(id)
 
   if(!data){
-    throw new Error("slide not fund")
+
+    throw createError(404, 'Slide not found')
   }
 
   return data
 }
 
 const remove = async (id) => {
-  await slidesRepository.remove(id);
+  const removedSlide = await slidesRepository.remove(id);
+
+
+  if(!removedSlide){
+    throw createError(400, "Slide couldn't be removed.")
+  }
+
 };
 
 
