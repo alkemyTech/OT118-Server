cd87d8f7aff5f84b67d2429f6e79b91aec44a853
Emmanuel Tabares <e.tabares20@gmail.com>
2022-01-07 17:41:10 -0300

    Swagger implementation for Docs (#60)
    * Swagger implementation for Docs
    
    * fixes

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const options = require('./docs/swaggerOptions');
 require('dotenv').config();
 
 const indexRouter = require('./routes/index');
+const SPECS = swaggerJsDoc(options)
 
 const app = express();
 app.use(cors());
@@ -22,6 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api/docs', swaggerUI.serve , swaggerUI.setup(SPECS))
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
