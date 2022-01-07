ff814dbcaf93c793d89af7b9c94e878ef5681661
Sebastián Arca <sebasarca711@gmail.com>
2022-01-07 17:11:34 -0300

    Fix getAll Users extra route.

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const authMiddleware = require('../middlewares/auth');
+const paginationMiddleware = require('../middlewares/pagination')
 const usersController = require('../controllers/users');
 
 router.delete('/:id', authMiddleware.inOwnUser, usersController.remove);
-router.get('/', authMiddleware.isAdmin, usersController.getAll);
-
-const paginationMiddleware = require('../middlewares/pagination')
-
 router.get('/', authMiddleware.isAdmin, paginationMiddleware.validator, usersController.getAll);
 
 module.exports = router;
