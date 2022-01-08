a57c3d441fb5573167bc0ea8faae7edf6f8a566f
Sebastián Arca <sebasarca711@gmail.com>
2022-01-08 17:02:41 -0300

    Fix Organization Message

--- controllers/organizations.js
+++ controllers/organizations.js
@@ -6,7 +6,7 @@ const update = async (req, res, next) => {
     const id = configuration.organizationId;
     const data = await organizationService.update(id, req.body);
     res.status(200).json({
-      msg: `Member updated successfully`,
+      msg: `Organization updated successfully`,
       data: data,
     });
   }
