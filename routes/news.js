47ce1d34d62033299b796510d2d19e61ea7b7a97
Sebastián Arca <sebasarca711@gmail.com>
2022-01-08 14:06:25 -0300

    Fix getCommentsByNews

--- routes/news.js
+++ routes/news.js
@@ -5,11 +5,13 @@ const router = express.Router();
 const newsController = require('../controllers/news');
 const newsMiddleware = require('../middlewares/news');
 const authMiddleware = require('../middlewares/auth');
+const commentsController = require("../controllers/comments");
 
 router.post('/', authMiddleware.isAdmin, newsMiddleware.inputValidation, newsController.create);
 router.get('/', authMiddleware.isAuth, newsController.getAll);
 router.delete('/:id', authMiddleware.isAdmin, newsController.remove);
 router.get('/:id', authMiddleware.isAdmin, newsController.getById);
 router.put('/:id', authMiddleware.isAdmin, newsMiddleware.inputValidation, newsController.update);
+router.get('/:id/comments', authMiddleware.isAuth, commentsController.getCommentsByNews);
 
 module.exports = router;
