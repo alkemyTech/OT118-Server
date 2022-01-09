1b069f19a9247857643c103924a6631df2c33d2e
IvanStacul <stacul.ivan98@gmail.com>
2022-01-09 10:48:48 -0300

    change error remove user

--- services/users.js
+++ services/users.js
@@ -1,13 +1,13 @@
 const createError = require("http-errors");
 const usersRepository = require("../repositories/users");
 const rolesRepository = require("../repositories/roles");
-const organizationRepository = require('../repositories/organizations');
+const organizationRepository = require("../repositories/organizations");
 const bcrypt = require("bcryptjs");
 const { generateToken } = require("../modules/auth");
 const { paginate } = require("../modules/pagination");
-const {send} = require('../modules/emailSender');
-const {createWecolmeEmailTemplate} = require('../modules/welcomeEmailTemplate');
-const config = require('../config/config');
+const { send } = require("../modules/emailSender");
+const { createWecolmeEmailTemplate } = require("../modules/welcomeEmailTemplate");
+const config = require("../config/config");
 
 const invalidUserMsg = "email or password is invalid.";
 const pageLimit = 10;
@@ -24,34 +24,34 @@ const create = async (user) => {
   await send(data.email, template, "¡Bienvenido!");
 
   return generateToken({ id: data.id });
-}
-
-const login = async (body) => {
-    const user = await usersRepository.findByEmail(body.email);
-    if (!user) throw createError(401, invalidUserMsg);
-    if (!bcrypt.compareSync(body.password, user.password)) throw createError(401, invalidUserMsg);
-    return generateToken({ id: user.id });
 };
 
-const getAll = async ({baseUrl, page}) => {
-    const count = await usersRepository.count();
-    const paginatedResult = await paginate(baseUrl,page, pageLimit, count);
-    if (count > 0) {
-        paginatedResult.data = await usersRepository.getAll(pageLimit, paginatedResult.offset);
-    }
-    delete paginatedResult.offset;
-    return paginatedResult;
+const login = async (body) => {
+  const user = await usersRepository.findByEmail(body.email);
+  if (!user) throw createError(401, invalidUserMsg);
+  if (!bcrypt.compareSync(body.password, user.password))
+    throw createError(401, invalidUserMsg);
+  return generateToken({ id: user.id });
+};
+
+const getAll = async ({ baseUrl, page }) => {
+  const count = await usersRepository.count();
+  const paginatedResult = await paginate(baseUrl, page, pageLimit, count);
+  if (count > 0) {
+    paginatedResult.data = await usersRepository.getAll(
+      pageLimit,
+      paginatedResult.offset
+    );
+  }
+  delete paginatedResult.offset;
+  return paginatedResult;
 };
 
 const remove = async (id) => {
-  const user = await usersRepository.getById(id)
-  if (!user){
-    const error = new Error(`User doesn't exist`)
-    error.status = 404
-    throw error
-  }
-  return await usersRepository.remove(id)
-}
+  const user = await usersRepository.getById(id);
+  if (!user) throw createError(404, { msg: "User doesn't exist" });
+  return await usersRepository.remove(id);
+};
 
 const getProfile = async (id) => {
   const data = await usersRepository.getById(id);
@@ -71,18 +71,18 @@ const update = async (id, body) => {
     body.password = bcrypt.hashSync(body.password, 10);
     await usersRepository.update(id, body);
     const userUpdated = await usersRepository.getById(id);
-    return userUpdated
-  }  else {
-      throw createError(404, { msg: "User not found" });
+    return userUpdated;
+  } else {
+    throw createError(404, { msg: "User not found" });
   }
 };
 
 module.exports = {
-    create,
-    login,
-    update,
-    getProfile,
-    getById,
-    remove,
-    getAll,
+  create,
+  login,
+  update,
+  getProfile,
+  getById,
+  remove,
+  getAll,
 };
