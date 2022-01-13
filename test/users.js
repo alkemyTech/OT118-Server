c643535fbb6d6a5db193c46fe7583fcf8ac4e08f
Tabitoo <lautarovaldez4@gmail.com>
2022-01-13 11:00:06 -0300

    added update a user test, not completed yet

--- test/users.js
+++ test/users.js
@@ -27,15 +27,9 @@ describe('Users endpoint', function() {
     })
 
     context('Users service', function() {
-        
-        const userToGet = {
-            firstName : 'pepe',
-            email : 'email@gmail.com',
-            image : 'image.png'
-        }
 
         const validUser = {
-            id: 40,
+            id: 53,
             fistName: "pepe",
             lastName: "pepito",
             email: "https://image.url.com",
@@ -44,6 +38,20 @@ describe('Users endpoint', function() {
             roleId: 2,
             createdAt: "2021-12-22T13:44:07.000Z",
             updatedAt: "2021-12-27T13:39:29.000Z"
+        };
+        
+        const userToGet = {
+            firstName : 'pepe',
+            email : 'email@gmail.com',
+            image : 'image.png'
+        }
+
+        const userToUpdate = {
+            fistName: "pepe",
+            lastName: "pepito",
+            email: "https://image.url.com",
+            password: '$2a$10$9GeXoYVeYveqVS15r4VI6.aRwzO1lAWOIlvWTInd7v0N4Ug8qCZ26',
+            image: 'image.png',
         };
 
         describe('get all with pagination', function() {
@@ -86,10 +94,28 @@ describe('Users endpoint', function() {
             })
             it('should throw user not found error', async function() {
                 usersMockedRepo.expects('getById').withExactArgs(validUser.id).returns(null);
-                usersMockedRepo.expects(methodToCall).withExactArgs(validUser.id).returns(undefined);
+                //usersMockedRepo.expects('remove').withExactArgs(validUser.id).returns(undefined);
                 await asyncErrorExpect(() => usersService.remove(validUser.id), expectedErrors.usersNotFound)
 
             }) 
+
+        })
+        
+        describe('update a user', function() {
+            it('should update a user', async function() {
+                usersMockedRepo.expects('getById').withExactArgs(validUser.id).atLeast(2).returns(validUser);
+                usersMockedRepo.expects('update').withExactArgs(validUser.id, userToUpdate).returns(validUser)
+
+                const userUpdated = await usersService.update(validUser.id,userToUpdate)
+
+                expect(userUpdated).equal(validUser)
+            })
+            it('should throw user not found error', async function() {
+                usersMockedRepo.expects('getById').withExactArgs(validUser.id).returns(null);
+                await asyncErrorExpect(() => usersService.update(validUser.id,userToUpdate), expectedErrors.usersNotFound)
+
+
+            })
 
         })
         
@@ -106,10 +132,8 @@ const asyncErrorExpect = async (method, expectedError) => {
     }
     expect(error).to.be.an('Error');
     if (expectedError) {
-        if (error.msg)
-            expect(error.msg).to.equal(expectedError.msg);
-        else
-            expect(error.message).to.equal(expectedError.msg);
+        if (error.msg) expect(error.msg).to.equal(expectedError.msg);
+        else expect(error.message).to.equal(expectedError.msg);
         if (error.status) {
             expect(error.status).to.be.equal(expectedError.status);
         }
