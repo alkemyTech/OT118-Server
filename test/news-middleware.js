5e07bc638fda15807528b4faf55cbc22b0f1aa5f
Sebastián Arca <sebasarca711@gmail.com>
2022-01-13 16:48:23 -0300

    Test Middleware News

--- test/news-middleware.js
+++ test/news-middleware.js
@@ -7,6 +7,11 @@ const {asyncValidation, getSpecificError} = require("./middleware-utils");
 
 describe("News Middleware", () => {
     let params;
+    const baseNovelty = {
+        name: "Novelty",
+        content: "Something Happen in day 2",
+        categoryId: 1
+    }
     beforeEach(() => {
         params = {
             req: {},
@@ -19,33 +24,25 @@ describe("News Middleware", () => {
     })
     it('should validate and call next without args', async function () {
         // Correct Case
-        params.req.body = {
-            name: "NoveltyTitle",
-            content: "Something Happen",
-            categoryId: 1,
-        }
+        params.req.body = {...baseNovelty}
         const error = await asyncValidation(params, inputValidation)
         expect(params.next.threw("BadRequestError")).to.be.false
         expect(params.next.alwaysCalledWith()).to.be.true
 
         expect(error).to.be.null
     })
-    it('should throw error', async function () {
+    it('should throw validation errors', async function () {
         /// Error case
-        params.req.body = {
-            content: "Something Happen",
-            categoryId: 1,
-        }
+        params.req.body = {}
+
         const error = await asyncValidation(params, inputValidation)
         expect(params.next.threw("BadRequestError")).to.be.true
         expect(error.msg).to.equal("Input validation error")
     })
-    it('should throw name length is too short error', async function () {
-        params.req.body = {
-            name: "No",
-            content: "Something Happen",
-            categoryId: 1,
-        }
+    it('should throw name length is too short', async function () {
+        params.req.body = {...baseNovelty}
+        params.req.body.name = "No";
+
         const error = await asyncValidation(params, inputValidation)
 
         expect(params.next.threw("BadRequestError")).to.be.true
@@ -54,5 +51,18 @@ describe("News Middleware", () => {
         const nameError = getSpecificError(error, "name");
         expect(nameError).to.be.an('object');
         expect(nameError.msg).to.equal("name must be between 3 to 255 characters long");
+    })
+    it('should throw content length is too short', async function () {
+        params.req.body = {...baseNovelty}
+        params.req.body.content= "Cont";
+
+        const error = await asyncValidation(params, inputValidation)
+
+        expect(params.next.threw("BadRequestError")).to.be.true
+        expect(error.errors).to.be.an('array')
+
+        const nameError = getSpecificError(error, "content");
+        expect(nameError).to.be.an('object');
+        expect(nameError.msg).to.equal("content must at least 5 characters long");
     })
 })
